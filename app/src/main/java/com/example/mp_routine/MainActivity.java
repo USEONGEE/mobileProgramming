b0423d586981f89d0aed09bd02ca71a263f0e9c7
jioh <ajy2277@naver.com>
2023-05-02 01:25:16 +0900

    commit

--- app/src/main/java/com/example/mp_routine/MainActivity.java
+++ app/src/main/java/com/example/mp_routine/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.mp_routine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +23,7 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),beginner.class);
-                startActivityForResult(intent,101);
+                loadFragment(new FirstFragment());
             }
         });
 
@@ -27,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loadFragment(new SecondFragment());
             }
         });
 
@@ -35,8 +39,15 @@ public class MainActivity extends AppCompatActivity {
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loadFragment(new ThirdFragment());
             }
         });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/mp_routine/SecondFragment.java
@@ -0,0 +1,21 @@
+package com.example.mp_routine;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SecondFragment extends Fragment {
+    public SecondFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+}
