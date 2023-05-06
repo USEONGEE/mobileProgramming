7e788ba443e90f5d0fc2dc14ceb55c6432f1e220
useongee <118250509+USEONGEE@users.noreply.github.com>
2023-05-06 15:30:07 +0900

    네비게이션 바 에러 수정
    xml 파일에서 네비바 뷰를 제일 하단에 배치함

--- app/src/main/java/com/example/ucanhealth/MainActivity.java
+++ app/src/main/java/com/example/ucanhealth/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ucanhealth;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,6 +16,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -39,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.InitializeLayout();
+        InitializeLayout();
 
         init();
         addRoutineBtn.setOnClickListener(openExerciseSettingDialog);
@@ -60,9 +63,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ActionBar actionBar = getSupportActionBar();
+
         //App Bar의 좌측 영영에 Drawer를 Open 하기 위한 Icon 추가
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menuicon);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.menuicon);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
@@ -74,28 +79,29 @@ public class MainActivity extends AppCompatActivity {
                 R.string.closed
         );
         drawer.addDrawerListener(actionBarDrawerToggle);
-
         // navigation 객체에 nav_view의 참조 반환
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         // navigation 객체에 이벤트 리스너 달기
-        navigationView.setNavigationItemSelectedListener(menuItem -> {
-            Log.i("clicked","success");
-            switch (menuItem.getItemId())
-            {
-                case R.id.menuitem1:
-                    Toast.makeText(getApplicationContext(), "SelectedItem 1", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.menuitem2:
-                    Toast.makeText(getApplicationContext(), "SelectedItem 2", Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.menuitem3:
-                    Toast.makeText(getApplicationContext(), "SelectedItem 3", Toast.LENGTH_SHORT).show();
-                    break;
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Log.i("clicked",String.valueOf(menuItem.getItemId()) + " selected");
+                switch (menuItem.getItemId())
+                {
+                    case R.id.menuitem1:
+                        Toast.makeText(getApplicationContext(), "SelectedItem 1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menuitem2:
+                        Toast.makeText(getApplicationContext(), "SelectedItem 2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menuitem3:
+                        Toast.makeText(getApplicationContext(), "SelectedItem 3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                drawer.closeDrawer(GravityCompat.START);
+                return false;
             }
-            drawer.closeDrawer(GravityCompat.START);
-            return true;
         });
-
     }
 
     @Override
