39b92ea49d5a01f821bcb198a169b148c1e77c70
jioh-ahn <81916752+jioh-ahn@users.noreply.github.com>
2023-05-29 16:00:43 +0900

    루틴 파트

---
+++ app/src/main/java/com/example/ucanhealth/menu_Routine.java
@@ -0,0 +1,14 @@
+package com.example.ucanhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_Routine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_routine);
+    }
+}
